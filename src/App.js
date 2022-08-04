d2b83ae85128e8bfe16f16a32a7d9a42032276b7
paivabenja <paivabenja05@gmail.com>
2022-08-04 20:06:42 -0300

    btns r defined bfore input

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import "./App.css";
 import BtnComp from "./components/BtnComp.js";
 
 function App() {
+  const input = document.getElementById("input");
   return (
     <div className="App">
-      <form action="submit">
-        <input type="number" placeholder="Insert a number" id="input" />
-        <BtnComp value="1"></BtnComp>
-        <BtnComp value="2"></BtnComp>
-        <BtnComp value="3"></BtnComp>
-        <BtnComp value="4"></BtnComp>
-        <BtnComp value="5"></BtnComp>
-        <BtnComp value="6"></BtnComp>
-        <BtnComp value="7"></BtnComp>
-        <BtnComp value="8"></BtnComp>
-        <BtnComp value="9"></BtnComp>
-        <BtnComp value="0"></BtnComp>
-        <BtnComp value="*"></BtnComp>
-        <BtnComp value="-"></BtnComp>
-        <BtnComp value="+"></BtnComp>
-        <BtnComp value="/"></BtnComp>
-        <BtnComp value="="></BtnComp>
+      <form>
+        <input type="number" placeholder="Insert a numberi" id="input" />
+        <BtnComp input={input} value="1"></BtnComp>
+        <BtnComp input={input} value="2"></BtnComp>
+        <BtnComp input={input} value="3"></BtnComp>
+        <BtnComp input={input} value="4"></BtnComp>
+        <BtnComp input={input} value="5"></BtnComp>
+        <BtnComp input={input} value="6"></BtnComp>
+        <BtnComp input={input} value="7"></BtnComp>
+        <BtnComp input={input} value="8"></BtnComp>
+        <BtnComp input={input} value="9"></BtnComp>
+        <BtnComp input={input} value="0"></BtnComp>
+        <BtnComp input={input} value="*"></BtnComp>
+        <BtnComp input={input} value="-"></BtnComp>
+        <BtnComp input={input} value="+"></BtnComp>
+        <BtnComp input={input} value="/"></BtnComp>
+        <BtnComp input={input} value="="></BtnComp>
       </form>
     </div>
   );
--- src/components/BtnComp.js
+++ src/components/BtnComp.js
@@ -1,17 +1,95 @@
 import React from "react";
 
 const BtnComp = (props) => {
-  const pressButton = (prop) => {
-    if (prop.isNaN) {
-      console.log(prop);
+  const operar = (numero1, numero2, tipoDeOperacion, resultado) => {
+    switch (tipoDeOperacion) {
+      case "*":
+        resultado = numero1 * numero2;
+        break;
+      case "/":
+        resultado = numero1 / numero2;
+        break;
+      case "+":
+        resultado = numero1 + numero2;
+        break;
+      case "-":
+        resultado = numero1 - numero2;
+        break;
+      default:
+        break;
     }
   };
+
+  const changeValue = (val) => {
+    props.numInput.value = val;
+  };
+
+  const addNum = (text, num) => {
+    num = +num;
+    num = text * 10 + num;
+    changeValue(num);
+  };
+
+  const pressButton = (prop) => {
+    let num1;
+    let num2;
+    let operacion;
+
+    if (+prop >= 0 && +prop <= 9) {
+      console.log(props.numInput);
+      addNum(props.numInput.value, prop);
+      return;
+    }
+
+    switch (prop) {
+      case "*":
+        num1 = props.numInput.value;
+        operacion = "*";
+        props.numInput.value = "";
+        props.numInput.placeholder = num1;
+        break;
+
+      case "/":
+        num1 = props.numInput.value;
+        operacion = "/";
+        props.numInput.value = "";
+        props.numInput.placeholder = num1;
+        break;
+
+      case "+":
+        num1 = props.numInput.value;
+        operacion = "+";
+        props.numInput.value = "";
+        props.numInput.placeholder = num1;
+        break;
+
+      case "-":
+        num1 = props.numInput.value;
+        operacion = "-";
+        props.numInput.value = "";
+        props.numInput.placeholder = num1;
+        break;
+
+      case "=":
+        num2 = props.numInput.value;
+        props.numInput.value = "";
+        operar(num1, num2, operacion, props.numInput.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={"btn-div btn" + props.value}>
       <button
         className="btn"
         id={"btn" + props.value}
-        onClick={pressButton(props.value)}
+        onClick={(x) => {
+          x.preventDefault();
+          pressButton(props.value);
+        }}
       >
         {props.value}
       </button>
